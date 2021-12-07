16970231ea4fcf1776794c3ee68962fa686bff41
Rafael Leite de Sá <rafaelleite012@gmail.com>
2021-12-06 21:25:18 -0300

    Initial

---
+++ ContadorVotos/src/view/Principal.java
@@ -0,0 +1,18 @@
+
+package view;
+
+/**
+ *
+ * @author Rafael Leite 
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // instancia o formulário, utilizando objeto anônimo
+        new FormContador();
+    }
+    
+}
